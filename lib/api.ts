a40f1d73a3369bfb3387cf0aabc331ec140c95a3
Marvin Arnold <marvin@geeky.rocks>
2025-02-09 04:28:31 -0600

    feat: remove terms

--- lib/api.ts
+++ lib/api.ts
@@ -8,19 +8,6 @@ export const TOKEN_DECIMALS = 18;
 export const USDC_DECIMALS = 6;
 export const CHAIN = baseSepolia;
 
-export type ICreateListingRequest = {
-    sellTokenAddress: `0x${string}`;
-    // "1000000000000000000000000" = 1.00 (shifted right by TOKEN_DECIMALS)
-    // number of tokens being sold
-    sellTokenAmount: string;
-
-    // "1000000" = 1.000000 USDC (shifted right by USDC_DECIMALS)
-    // USDC price for each whole 1.0 token (not for 0.000000000000001 tokens)
-    sellTokenPrice: string;
-    offerExpiresAt: number;
-    termsDeliverables: string;
-}
-
 export type IListing = {
     id: string;
     content: string;
@@ -34,7 +21,6 @@ export type IListing = {
     // USDC price for each whole 1.0 token (not for 0.000000000000001 tokens)
     sellTokenPrice: string;
     offerExpiresAt: number;
-    termsDeliverables: string;
     // In usdc $1 = 1000000
     receiveTokenAmount: string;
     receiveTokenAddress: `0x${string}`;
@@ -42,10 +28,4 @@ export type IListing = {
     purchaseTxHash?: `0x${string}`;
     purchaserAddress?: `0x${string}`;
     purchasedAt?: number;
-}
-
-export type ICreateListingResponse = {
-    success: boolean;
-    listing: IListing;
-    error?: string;
 }
